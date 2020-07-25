65b329ab9507ab5c8c2ba79eb8f2802f45fd4745
D_SM <dharamsinghmeena2000@gmail.com>
2020-07-25 11:46:43 +0530

    Update longest increasing sequence.cpp

--- Dynamic programming/longest increasing sequence.cpp
+++ Dynamic programming/longest increasing sequence.cpp
@@ -13,11 +13,10 @@ int n ;
 int longestsequence(int arr[])
 {
     int lis[n];
-    for (int i = 0; i < n; i++) 
-       lis[i] = 1;
     lis[0] = 1;
 
     for (int i = 0; i < n; i++){   // fix ending point
+        lis[i] = 1;
         for (int j = 0; j < i ; j++) 
         {
             if (arr[j] < arr[i])
@@ -25,7 +24,7 @@ int longestsequence(int arr[])
         }
     }  
     
-    int ans = 1;
+    int ans = 1;   // shortest inc sequence
     for (int i = 0; i < n; i++)  
         ans = max(ans, lis[i]);
     return ans;
