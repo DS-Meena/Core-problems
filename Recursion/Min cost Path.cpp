5195d731518bc0e30f8628d65f40dbcc74a07416
D_SM <dharamsinghmeena2000@gmail.com>
2020-07-26 15:40:15 +0530

    Create Min cost Path.cpp

---
+++ Recursion/Min cost Path.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+const int N = 100; 
+const int mod = 1e9 + 7;
+
+int n, m;
+
+int minCost(int mat[][4], int i, int j)
+{
+    if (i == n && j == m) 
+        return mat[i][j];
+    if (i > n || j > m)   // not choose this path
+        return INT_MAX;
+    
+    int x = minCost(mat, i, j + 1);
+    int y = minCost(mat, i + 1, j);
+    int z = minCost(mat, i+1, j + 1); 
+    int minimum = min(x, min(y, z));   
+    
+    return (mat[i][j] + minimum );
+}
+
+void test_case()
+{ 
+    int mat[][4] = {
+        {5, 3, 4, 10},
+        {8, 2, 9, 5},
+        {10, 0, 6, 8}
+    };
+    n = sizeof(mat) / sizeof(mat[0]); 
+    m = sizeof(mat[0]) / sizeof(mat[0][0]);
+    n--, m--;
+    cout << minCost(mat, 0, 0);
+
+    cout << endl;
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve()
+{
+    int t;
+    t = 1;
+    // cin >> t;
+    for (int nr = 1; nr <= t; nr++)
+    {
+        test_case();
+    }
+}
+
+int main()
+{   
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    //synchronize c++ streams with c streams
+    ios_base::sync_with_stdio(false);
+    //guarentes flushing
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+}
