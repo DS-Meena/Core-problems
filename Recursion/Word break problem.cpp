ba398b884c019761c8f2160ce4a312397251982f
D_SM <dharamsinghmeena2000@gmail.com>
2020-07-28 14:22:10 +0530

    Create Word break problem.cpp

---
+++ Recursion/Word break problem.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+const int N = 100; 
+const int mod = 1e9 + 7;
+
+int n, m;
+string wordDict[] = {"Hard", "Things", "Thing", "About"};
+
+bool dictionaryContains(string word)
+{
+    m = sizeof(wordDict) / sizeof(wordDict[0]);
+
+    for (int i =0; i < m; i++)
+        if (wordDict[i].compare(word) == 0) 
+            return true;
+    return false;
+}
+
+bool canBreaked(string str)
+{
+    int curr_size = str.length();  // size keeps changing 
+
+    if (curr_size == 0)
+        return true;
+
+    for (int i = 1; i <= curr_size; i++)
+    {
+        if (dictionaryContains(str.substr(0, i)) &&
+            canBreaked(str.substr(i, curr_size - i)))
+            return true;
+    }
+    return false;
+}
+
+void test_case()
+{
+    string s = "HardThingAboutHardThings";
+    if (canBreaked(s))
+        cout << "YES";
+    else 
+        cout << "NO";
+
+    cout << endl;
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve()
+{
+    int t;
+    t = 1;
+    // cin >> t;
+    for (int nr = 1; nr <= t; nr++)
+    {
+        test_case();
+    }
+}
+
+int main()
+{   
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    //synchronize c++ streams with c streams
+    ios_base::sync_with_stdio(false);
+    //guarentes flushing
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+}
