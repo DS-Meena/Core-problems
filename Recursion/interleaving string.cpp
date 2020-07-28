5785bed30bf16ca43bca03291c0203e818ce2242
D_SM <dharamsinghmeena2000@gmail.com>
2020-07-28 11:26:15 +0530

    Update interleaving string.cpp

--- Recursion/interleaving string.cpp
+++ Recursion/interleaving string.cpp
@@ -9,7 +9,8 @@ const int N = 100;
 const int mod = 1e9 + 7;
 
 int n, m;
-
+/*
+// 1st technique
 bool caninterleave(char* a, char* b, char* c)
 {
     if (!(*a || *b || *c)) 
@@ -35,6 +36,52 @@ void test_case()
     else 
          cout << "NO";
 
+    cout << endl;
+    return;
+}
+*/
+
+// 2nd technique
+int o;
+
+bool checkinterleave(string s1, string s2, string s3, int i, int j, int k)
+{  
+    if (k >= o) 
+       return true;  
+    
+    bool check1 = checkinterleave(s1, s2, s3, i + 1, j, k + 1);
+    bool check2 = checkinterleave(s1, s2, s3, i, j + 1, k + 1);
+    
+    if ((s1[i] == s3[k]) && check1)
+        return true;
+    else if ((s2[j] == s3[k]) && check2)
+        return true;
+    else 
+        return false;
+    //return ((s1[i] == s3[k]) && check1) || ((s2[j] == s3[k]) && check2);
+}
+
+bool caninterleave(string s1, string s2, string s3)
+{  
+    n = s1.length();
+    m = s2.length();
+    o = s3.length();
+
+    if (n + m != o) 
+        return false;
+    if (!(o || n || m)) 
+        return true;
+    return checkinterleave(s1, s2, s3, 0, 0, 0); 
+}
+
+void test_case()
+{
+    string s1 = "X", s2 = "YX", s3 = "XXY";
+    if (caninterleave(s1, s2, s3)) 
+        cout << "YES";
+    else 
+        cout << "NO"; 
+    
     cout << endl;
     return;
 }
