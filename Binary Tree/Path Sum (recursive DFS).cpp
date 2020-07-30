a3d76fb22c9dfdfa18bbe55a4dc18f0ccc9c0dab
D_SM <dharamsinghmeena2000@gmail.com>
2020-07-30 16:35:30 +0530

    Create Path Sum (recursive DFS).cpp

---
+++ Binary Tree/Path Sum (recursive DFS).cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+const int N = 100; 
+const int mod = 1e9 + 7;
+
+int n, m;
+
+struct Node
+{
+    Node *left, *right;
+    int key;
+};
+
+Node *newNode(int key)
+{
+    Node *temp = new Node;
+    temp->key = key;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+bool pathExists(Node* root, int sum)
+{
+    if (root == NULL)
+        return (sum == 0);
+
+    else
+    {
+        bool ans = 0;
+
+        int subSum = sum - root->key;
+
+        if (subSum == 0 && root->left == NULL && root->right == NULL)
+            return 1;
+
+        if (root->left)
+            ans = ans || pathExists(root->left, subSum);
+        if (root->right)
+            ans = ans || pathExists(root->right, subSum);
+
+        return ans;
+    }
+}
+
+void test_case()
+{
+    int sum = 22;
+
+    Node *root = newNode(5);
+    root->left = newNode(4);
+    root->right = newNode(8);
+    root->left->left = newNode(11);
+    root->right->left = newNode(13);
+    root->right->right = newNode(4);
+    root->left->left->left = newNode(7);
+    root->left->left->right = newNode(2);
+    root->right->right->right = newNode(1);
+
+    if (pathExists(root, sum))
+        cout << "YES";
+    else 
+        cout << "NO"; 
+
+    cout << endl;
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve()
+{
+    int t;
+    t = 1;
+    // cin >> t;
+    for (int nr = 1; nr <= t; nr++)
+    {
+        test_case();
+    }
+}
+
+int main()
+{   
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    //synchronize c++ streams with c streams
+    ios_base::sync_with_stdio(false);
+    //guarentes flushing
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+}
