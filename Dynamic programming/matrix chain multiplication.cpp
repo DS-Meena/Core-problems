fc7857b3bf7c0c7c5d0c77dd4aec65cf1313430f
D_SM <dharamsinghmeena2000@gmail.com>
2020-07-30 21:45:46 +0530

    Update matrix chain multiplication.cpp

--- Dynamic programming/matrix chain multiplication.cpp
+++ Dynamic programming/matrix chain multiplication.cpp
@@ -12,19 +12,18 @@ int n ;
 
 int chainmultiplication(int arr[])
 { 
-    int dp[n][n];    //one extra row and columns
+    int dp[n][n];  
 
-    for (int i =1; i < n; i++)    // one matrix 
+    for (int i =1; i < n; i++)  //single matrix 
         dp[i][i] = 0;
     
     // L is chain length
     for (int L = 2; L < n; L++){
-        int w = n - L + 1;
-        for (int i = 1; i < w; i++)
+        for (int i = 1; i < n - L + 1; i++)  //starting index
         {
-            int j = i + L - 1;
+            int j = i + L - 1;   //ending index
             dp[i][j] = INT_MAX;
-            for (int k = i; k <= j -1; k++) 
+            for (int k = i; k < j; k++) 
             {
                 dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] +
                                 arr[i - 1] * arr[k] * arr[j]);
