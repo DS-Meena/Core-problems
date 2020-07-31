e993723afe4b85cfa39221fe1ded92e26984c10b
D_SM <dharamsinghmeena2000@gmail.com>
2020-07-31 15:42:53 +0530

    Create Min Depth of BT(recursive).cpp

---
+++ Binary Tree/Min Depth of BT(recursive).cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+const int N = 100; 
+const int mod = 1e9 + 7;
+
+int n, m;
+
+struct node
+{
+    node *left, *right;
+    int key;
+};
+
+node* newnode(int value)
+{
+    node* NN = new node;
+    NN->key = value;
+    NN->left = NULL;
+    NN->right = NULL;
+    return NN; 
+}
+
+int minDepth(node* root, int curr_depth)
+{
+    if (root->left == NULL && root->right == NULL) 
+       return 1;
+     
+    int left_depth = minDepth(root->left, curr_depth);
+    int right_depth = minDepth(root->right, curr_depth);
+    
+
+    return min(left_depth, right_depth) + 1;
+}
+
+void test_case()
+{
+    node* root = newnode(10);
+    root->left = newnode(5);
+    root->right = newnode(15);
+    root->left->left = newnode(11);
+    root->left->right = newnode(6);
+
+    cout << minDepth(root, 0);
+
+    cout << endl;
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve()
+{
+    int t;
+    t = 1;
+    // cin >> t;
+    for (int nr = 1; nr <= t; nr++)
+    {
+        test_case();
+    }
+}
+
+int main()
+{   
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    //synchronize c++ streams with c streams
+    ios_base::sync_with_stdio(false);
+    //guarentes flushing
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+}
