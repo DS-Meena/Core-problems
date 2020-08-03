b817c688fb2063b43eca498c51dfdd227597c72e
D_SM <dharamsinghmeena2000@gmail.com>
2020-08-03 22:06:28 +0530

    Create minimum coin change.cpp

---
+++ Recursion/minimum coin change.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+const int N = 100; 
+const int mod = 1e9 + 7;
+
+int n, m;
+
+int minCoins(int coins[], int A)
+{
+    if (A == 0)
+       return 0;
+
+    int min_c = INT_MAX; 
+    for (int i = 0; i< n; i++)
+    { 
+        if (coins[i] <= A)
+        {
+            int curr_min = minCoins(coins, A - coins[i]);
+            if (min_c > 1 + curr_min && curr_min != INT_MAX)
+                min_c = curr_min + 1;
+        }
+    }
+    return min_c;
+}
+
+void test_case()
+{
+    int coins[] = {2, 3, 5, 6};
+    int A = 10;
+
+    n = sizeof(coins) / sizeof(coins[0]);
+    cout << minCoins(coins, A);
+
+    cout << endl;
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve()
+{
+    int t;
+    t = 1;
+    // cin >> t;
+    for (int nr = 1; nr <= t; nr++)
+    {
+        test_case();
+    }
+}
+
+int main()
+{   
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    //synchronize c++ streams with c streams
+    ios_base::sync_with_stdio(false);
+    //guarentes flushing
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+}
