1eefe30c010c8e197b365d372207033116a17b0c
D_SM <dharamsinghmeena2000@gmail.com>
2020-08-15 13:28:59 +0530

    Create LRU Cache (Double ended queue using list).cpp

---
+++ Stack and Queue/LRU Cache (Double ended queue using list).cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+const int N = 100; 
+const int mod = 1e9 + 7;
+
+int n, m, k;
+
+class LRUCache {
+    public:
+        list <pair< int, int> > ls;
+        unordered_map <int, list <pair <int, int> > :: iterator> map;
+        int size;
+
+        LRUCache(int capacity)
+        {
+            size = capacity;
+        } 
+        int get(int key)            
+        {
+            if (map.find(key) == map.end()) 
+                return -1;
+            ls.splice(ls.begin(), ls, map[key]);  // moves to front because recently used
+            return map[key]->second;
+        }
+        void put(int key, int value)     
+        {
+            if (map.find(key) != map.end())
+            {
+                ls.splice(ls.begin(), ls, map[key]);  // moves to front because recently used
+                map[key]->second = value;
+                return;
+            }
+            if (ls.size() == size)
+            {
+                auto d_key = ls.back().first;        // removes from last because least recently used
+                ls.pop_back();
+                map.erase(d_key);
+            }
+            ls.push_front({key, value});
+            map[key] = ls.begin();
+        }
+};
+
+void test_case()
+{
+    LRUCache cache(3);
+    cache.put(1, 1);
+    cache.put(2, 2);
+    cache.put(1, 3);
+    cout << cache.get(1) << endl;
+    cache.put(3, 4);
+    cache.put(4, 3);
+    cout << cache.get(2) << endl;
+    cache.put(5, 5);
+    cout << cache.get(1) << endl;
+    
+    cout << endl;
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve()
+{
+    int t;
+    t = 1;
+    // cin >> t;
+    for (int nr = 1; nr <= t; nr++)
+    {
+        test_case();
+    }
+}
+
+int main()
+{   
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    //synchronize c++ streams with c streams
+    ios_base::sync_with_stdio(false);
+    //guarentes flushing
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+}
