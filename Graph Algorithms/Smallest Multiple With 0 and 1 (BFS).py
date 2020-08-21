81f6465a0a9789276dc68f3381fb47a16819991f
D_SM <dharamsinghmeena2000@gmail.com>
2020-08-21 14:53:22 +0530

    Create Smallest Multiple With 0 and 1 (BFS).py

---
+++ Graph Algorithms/Smallest Multiple With 0 and 1 (BFS).py
@@ -0,0 +1,18 @@
+def getMinimumMultipleOfBinaryDigit(num):
+    frontier = []
+
+    visited = set()
+    curr = '1'
+    frontier.append(curr)
+    while frontier:           # until frontier becomes empty
+        curr = frontier.pop()
+        remainder = int(curr) % num
+        if remainder == 0:
+            return curr
+        if remainder not in visited:
+            visited.add(remainder)
+            frontier.append(curr + '0')
+            frontier.append(curr + '1')
+
+n = 18
+print(getMinimumMultipleOfBinaryDigit(n))
