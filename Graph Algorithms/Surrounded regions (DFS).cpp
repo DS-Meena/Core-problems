23e476614a32809d539c338172995525e9e78a8b
D_SM <dharamsinghmeena2000@gmail.com>
2020-09-09 22:33:42 +0530

    Update Surrounded regions (DFS).cpp

--- Graph Algorithms/Surrounded regions (DFS).cpp
+++ Graph Algorithms/Surrounded regions (DFS).cpp
@@ -2,20 +2,20 @@
 def dfs(board, i, j):
     if board[i][j] == 'O':
         board[i][j] = '-'
-        for x , y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+        for x , y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:   # movement like respectable horse also works fine
             if 0 <= x < m and 0 <= y < n:
                 dfs(board, x, y) 
 
 def replaceSurrounded(board, m, n):
-    for i in range(m):
+    for i in range(m):             # make boundary O as '-'  , simple flood-fill algorithms also works fine
         dfs(board, i, 0)
         dfs(board, i, n - 1)
 
-    for i in range(n):
+    for i in range(n):            # make boundary O as '-'
         dfs(board, 0, i)
         dfs(board, m - 1, i) 
     
-    for i in range(m):
+    for i in range(m):            # make all '-' as O and remaining as X
         for j in range(n):
             if board[i][j] == '-':
                 board[i][j] = 'O'
