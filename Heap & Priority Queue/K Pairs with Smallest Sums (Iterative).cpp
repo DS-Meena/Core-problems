c86b13945ac7280d20090992867b77a6388d3405
D_SM <dharamsinghmeena2000@gmail.com>
2020-09-15 22:39:55 +0530

    Update K Pairs with Smallest Sums (Iterative).cpp

--- Heap & Priority Queue/K Pairs with Smallest Sums (Iterative).cpp
+++ Heap & Priority Queue/K Pairs with Smallest Sums (Iterative).cpp
@@ -12,15 +12,15 @@ int n, m;
 
 void kSmallestPair(int arr1[], int arr2[], int k)
 {
-    int index2[m];
+    int index2[n];    // which index to consider to arr2
     memset(index2, 0, sizeof(index2));
 
-    while (k > 0)
+    while (k > 0)     // untill find k smallest pairs
     {
         int min_sum = INT_MAX;
         int min_indx = 0;
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) {      // check each element of arr1 and its corresponding index2[i]th arr2 element
             if (index2[i] < n && arr1[i] + arr2[index2[i]] < min_sum)
             {
                 min_indx = i;
@@ -28,7 +28,7 @@ void kSmallestPair(int arr1[], int arr2[], int k)
             }
         }
         cout << arr1[min_indx] << " " << arr2[index2[min_indx]] << endl;
-        index2[min_indx]++;
+        index2[min_indx]++;     // already taken 
 
         k--;
     }
