5977744dc3822ced3dc5cf882079cb9d3368ef21
D_SM <dharamsinghmeena2000@gmail.com>
2020-09-18 22:51:21 +0530

    Update Convert A minHeap To maxHeap (maxHeapify).cpp

--- Heap & Priority Queue/Convert A minHeap To maxHeap (maxHeapify).cpp
+++ Heap & Priority Queue/Convert A minHeap To maxHeap (maxHeapify).cpp
@@ -16,15 +16,15 @@ void maxHeapify(int arr[], int i)
     int right = 2 * i + 2;
     int largest = i;
     
-    if (left < n && arr[left] > arr[largest]) 
+    if (left < n && arr[left] > arr[largest])    // find largest
         largest = left;
     if (right < n && arr[right] > arr[largest]) 
         largest = right;
     
     if (largest != i)    // if parent is not largest
     {
-        swap(arr[i], arr[largest]);
-        maxHeapify(arr, largest);
+        swap(arr[i], arr[largest]);             // swap parent with largest 
+        maxHeapify(arr, largest);              // recurssivley heapify the childs also
     }
 }
 
@@ -33,8 +33,8 @@ void test_case()
     int arr[] = {3, 5, 9, 6, 8, 20, 10, 12, 18, 9};  
     n = sizeof(arr) / sizeof(arr[0]);
 
-    for (int i = (n - 2) / 2; i >= 0; i--)   
-        maxHeapify(arr, i);
+    for (int i = (n - 2) / 2; i >= 0; i--)   // apply maxHeapify to all nodes except the leaf nodes
+        maxHeapify(arr, i);                 // i.e, from (n-2)/2 to 0th node
     
     for (int i = 0; i < n; i++) 
         cout << arr[i] << " ";
