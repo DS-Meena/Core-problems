059809b3bf6320a63b4945ff9e07a8bb6a93332f
D_SM <dharamsinghmeena2000@gmail.com>
2020-10-27 00:24:32 +0530

    Update Maximum Subarray Sum(Kadanes's).cpp

--- Dynamic programming/Maximum Subarray Sum(Kadanes's).cpp
+++ Dynamic programming/Maximum Subarray Sum(Kadanes's).cpp
@@ -12,15 +12,16 @@ int n, m;
 
 int maxSASum(int arr[])
 {
-    int max_so_far = 0, max_ending_here = 0;
+    int max_so_far = INT_MIN, max_ending_here = 0;
 
     for (int i = 0; i < n; i++)
     {
         max_ending_here += arr[i];
-        if (max_ending_here < 0) 
-            max_ending_here = 0;
         
         max_so_far = max(max_so_far, max_ending_here);
+        
+        if (max_ending_here < 0) 
+            max_ending_here = 0;
     }
     return max_so_far;
 }
